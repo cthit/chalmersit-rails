70a21134558e3307245f1a5ff5b10d1bf4f6e29e
Cannonbait <ivar_busig@hotmail.com>
2014-11-27 19:18:01 +0100

    Set up and fixed print script

--- app/helpers/print_helper.rb
+++ app/helpers/print_helper.rb
@@ -1,2 +1,13 @@
 module PrintHelper
+	DOMAIN = "remote1.student.chalmers.se"
+	SSH_FILENAME = ".print/chalmersit.dat"
+	#Called to upload and print a file on a chalmers printer
+	def print_script(username, password, filename, printer)
+		output = ""
+		Net::SSH.start(DOMAIN, username, password: password) do |ssh|
+			ssh.scp.upload!(filename, SSH_FILENAME)
+			output = ssh.exec! "export CUPS_GSSSERVICENAME=HTTP; /usr/bin/lpr -P #{printer} #{SSH_FILENAME}"
+		end
+		raise output unless output.nil?
+	end
 end
