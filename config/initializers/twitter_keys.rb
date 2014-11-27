28a5dbf8712866ce15ffe9d03970fbd158adfddb
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-11-27 01:07:27 +0100

    moved api_keys to secrets.yml

--- config/initializers/twitter_keys.rb
+++ config/initializers/twitter_keys.rb
@@ -1,7 +1,8 @@
 require 'twitter'
 $twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = '' # YOUR_CONSUMER_KEY
-  config.consumer_secret = '' # YOUR_CONSUMER_SECRET
-  config.access_token = '' # YOUR_OAUTH_TOKEN
-  config.access_token_secret = '' # YOUR_OAUTH_TOKEN_SECRET
+  twitter_settings = Rails.application.secrets.twitter
+  config.consumer_key = twitter_settings['consumer_key']
+  config.consumer_secret = twitter_settings['consumer_secret']
+  config.access_token = twitter_settings['access_token']
+  config.access_token_secret = twitter_settings['access_token_secret']
 end
