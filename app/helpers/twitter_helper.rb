bd36c48a90be3ffe28a42e0cbbe609ca34a02c93
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-11-27 02:50:50 +0100

    Fix replacing, added time value

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,21 +1,25 @@
 module TwitterHelper
   def twitter_feed(username)
     $twitter.user_timeline(username).map do |t|
-      format_tweet(t)
+      { html: format_tweet(t), tweet: t }
     end
   end
 
-  def format_tweet(tweet)
-    text = tweet.text.dup
-    tweet.hashtags.each do |tag|
-      text.sub! "\##{tag.text}", "<a href=\"http://twitter.com/search?q=#{tag.text}&src=hash\">\##{tag.text}</a>"
+  private
+
+    URLS = {
+      'Twitter::Entity::Hashtag' => -> (tag) { "<a href=\"http://twitter.com/search?q=#{tag.text}&src=hash\">\##{tag.text}</a>" },
+      'Twitter::Entity::URI' => -> (uri) { "<a href=\"#{uri.expanded_url}\">#{uri.display_url}</a>" },
+      'Twitter::Entity::UserMention' => -> (user) { "<a href=\"http://twitter.com/#{user.screen_name}\" title=\"#{user.name}\">@#{user.screen_name}</a>" }
+    }
+
+    def format_tweet(tweet)
+      text = tweet.text.dup
+
+      replaces = tweet.hashtags + tweet.uris + tweet.user_mentions
+      replaces.sort_by{ |r| -r.indices[0] }.each do |r|
+	text[(r.indices[0])...(r.indices[1])] = URLS[r.class.to_s].call(r)
+      end
+      text.html_safe
     end
-    tweet.uris.each do |uri|
-      text.sub! "#{uri.url}", "<a href=\"#{uri.expanded_url}\">#{uri.display_url}</a>"
-    end
-    tweet.user_mentions.each do |user|
-      text.sub! "@#{user.screen_name}", "<a href=\"http://twitter.com/#{user.screen_name}\" title=\"#{user.name}\">@#{user.screen_name}</a>"
-    end
-    text.html_safe
-  end
 end
