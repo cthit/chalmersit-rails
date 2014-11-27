2cd376261131259f7821eaa4ed9f0ba551bdacd9
Cannonbait <ivar_busig@hotmail.com>
2014-11-27 22:37:13 +0100

    Added view options for printing

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -10,11 +10,13 @@ class PrintController < ApplicationController
     pass = params[:password]
     file = params[:file]
     printer = params[:printer]
+    copies = params[:copies]
+    options = params[:options]
 
     filepath = file.tempfile.path
 
     begin
-      print_script user, pass, filepath, printer
+      print_script user, pass, filepath, printer, copies
       flash[:notice] = "Your document has been sent to the printer"
     rescue => e
       flash[:alert] = e.message
--- app/helpers/print_helper.rb
+++ app/helpers/print_helper.rb
@@ -1,13 +1,24 @@
 module PrintHelper
-	DOMAIN = "remote1.student.chalmers.se"
-	SSH_FILENAME = ".print/chalmersit.dat"
-	#Called to upload and print a file on a chalmers printer
-	def print_script(username, password, filename, printer)
-		output = ""
-		Net::SSH.start(DOMAIN, username, password: password) do |ssh|
-			ssh.scp.upload!(filename, SSH_FILENAME)
-			output = ssh.exec! "export CUPS_GSSSERVICENAME=HTTP; /usr/bin/lpr -P #{printer} #{SSH_FILENAME}"
-		end
-		raise output unless output.nil?
-	end
+  DOMAIN = "remote1.student.chalmers.se"
+  SSH_FILENAME = ".print/chalmersit.dat"
+  #Called to upload and print a file on a chalmers printer
+  def print_script(username, password, filename, printer, copies, options = {})
+    output = nil
+    Net::SSH.start(DOMAIN, username, password: password) do |ssh|
+      ssh.scp.upload!(filename, SSH_FILENAME)
+      puts "export CUPS_GSSSERVICENAME=HTTP; /usr/bin/lpr -P #{printer} -\# #{copies} #{stringify_options(options)} #{SSH_FILENAME}"
+      output = ssh.exec! "export CUPS_GSSSERVICENAME=HTTP; /usr/bin/lpr -P #{printer} -\# #{copies} #{stringify_options(options)} #{SSH_FILENAME}"
+    end
+    raise output unless output.nil?
+  end
+
+  def stringify_options(options)
+    str_options = []
+    options.each do |k, v|
+      str_options << "-o #{k}=#{v}"
+    end
+    str_options.join ' '
+  end
+
+
 end
