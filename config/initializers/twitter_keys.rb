8ad3bf1d45ea32b58b6ffdaf8e796f3a797ecd01
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-12-08 03:41:33 +0100

    do not use twitter in testing env

--- config/initializers/twitter_keys.rb
+++ config/initializers/twitter_keys.rb
@@ -1,8 +1,11 @@
 require 'twitter'
-$twitter = Twitter::REST::Client.new do |config|
-  twitter_settings = Rails.application.secrets.twitter
-  config.consumer_key = twitter_settings['consumer_key']
-  config.consumer_secret = twitter_settings['consumer_secret']
-  config.access_token = twitter_settings['access_token']
-  config.access_token_secret = twitter_settings['access_token_secret']
+
+unless Rails.env.test?
+  $twitter = Twitter::REST::Client.new do |config|
+    twitter_settings = Rails.application.secrets.twitter
+    config.consumer_key = twitter_settings['consumer_key']
+    config.consumer_secret = twitter_settings['consumer_secret']
+    config.access_token = twitter_settings['access_token']
+    config.access_token_secret = twitter_settings['access_token_secret']
+  end
 end
