a5b8187be49b5e2e3790429d8ca96a081467b11f
Meddan <christoffer.medin@gmail.com>
2014-12-09 19:25:30 +0100

    Finished work on courses.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-    has_and_belongs_to_many :period
+    has_and_belongs_to_many :periods
     validates :code, presence: true, uniqueness: true
     validates :name, presence: true
     validates :year, numericality: { only_integer: true , greater_than: 0 , less_than: 6}, allow_blank: true
