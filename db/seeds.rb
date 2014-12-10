f0a99609489b97c80ec293aafd38babea32c7de4
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-12-10 03:36:37 +0100

    reasonable seed data for committees

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ coms = [{:title=>"Styrelse", :name=>"styrIT"}, {:title=>"Studienämnd", :name=>"snIT"}, {:title=>"Sexmästeri", :name=>"sexIT"}, {:title=>"PR & Rustmästeri", :name=>"P.R.I.T."}, {:title=>"Mottagningskommitté", :name=>"NollKIT"}, {:title=>"Arbetsmarknad", :name=>"ArmIT"}, {:title=>"Digitala system", :name=>"digIT"}, {:title=>"Fanbärare", :name=>"FanbärerIT"}, {:title=>"Fysiska aktiviteter", :name=>"frITid"}, {:title=>"Digitala spel", :name=>"8-bIT"}, {:title=>"Analoga spel", :name=>"DrawIT"}, {:title=>"Foto", :name=>"FlashIT"}, {:title=>"Häfv och odygd", :name=>"HookIT"}, {:title=>"Sektionens revisorer", :name=>"Revisorer"}, {:title=>"Valberedning", :name=>"Valberedningen"}]
+
+ coms.each do |c|
+  c.merge!(slug: c[:name].gsub('.', '').parameterize)
+  Committee.create!(c)
+end
