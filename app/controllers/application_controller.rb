3cbdede6ebb171f93eed9c726acf479fc7025824
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-12-10 02:45:24 +0100

    footer + committees in footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_committees
+
+  private
+    def set_committees
+      @committees = Committee.all
+    end
 end
