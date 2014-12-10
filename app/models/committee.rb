2c29a30f92c694ec46e931a5d1140158be1133ef
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-12-10 04:45:35 +0100

    committee validations

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,4 +1,7 @@
 class Committee < ActiveRecord::Base
+  validates :slug, :name, :title, :description, :url, :email, presence: true
+  validates :slug, :name, uniqueness: true
+
   def to_param
     slug
   end
