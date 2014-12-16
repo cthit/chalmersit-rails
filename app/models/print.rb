26743ad5a6f094360c5dc46ea479aa80742fb34e
Cannonbait <ivar_busig@hotmail.com>
2014-12-16 21:29:41 +0100

    Added tests for printing and moved values for media and ppi to model

--- app/models/print.rb
+++ app/models/print.rb
@@ -16,21 +16,29 @@ class Print
   end
 
   def to_s
-    "/usr/bin/lpr -P #{printer} -\# #{copies} #{options}"
+    "/usr/bin/lpr -P '#{printer}' -\# #{copies} #{options}"
   end
 
   def options
-    [:duplex, :ranges, :ppi, :media].select{ |o| send o }.map { |opt| "-o #{opt}\=#{send opt}" }.join " "
+    [:duplex, :ranges, :ppi, :media].select{ |o| send o }.map { |opt| "-o #{opt}\='#{send opt}'" }.join " "
   end
 
   def duplex
     @duplex ? "two-sided-long-edge" : "one-sided"
   end
 
+  def self.available_media
+    %w(A3 A4)
+  end
+
+  def self.available_ppi
+    %w(auto 600 1200)
+  end
+
   validates :copies, numericality: { only_integer: true}
   validates :duplex, inclusion: { in: ["two-sided-long-edge", "one-sided"]}
-  validates :ranges, format:  { with: /[0-9-, ]+/, allow_blank: true}
-  validates :media, inclusion: { in: ["A4", "A3"]}
-  validates :ppi, inclusion: { in: ["auto", "600", "1200"]}
+  validates :ranges, format:  { with: /[0-9\-, ]+/, allow_blank: true}
+  validates :media, inclusion: { in: Print.available_media }
+  validates :ppi, inclusion: { in: Print.available_ppi }
   validates :username, :password, :file, :printer, presence: true
 end
--- spec/models/print_spec.rb
+++ spec/models/print_spec.rb
@@ -2,11 +2,18 @@ describe Print do
   it 'should print commandline options' do
     print = FactoryGirl.build(:print)
     print_str = print.to_s
+    expect(print_str).to include(" -P 'HUBBEN' ")
     expect(print_str).to include(' -# 100 ')
-    expect(print_str).to include(' -o duplex=two-sided-long-edge')
-    expect(print_str).to include(' -# 100 ')
-    expect(print_str).to include(' -# 100 ')
-    expect(print_str).to include(' -# 100 ')
+    expect(print_str).to include(" -o duplex='two-sided-long-edge'")
+    expect(print_str).to include(" -o ranges='3, 5-7'")
+    expect(print_str).to include(" -o ppi='600'")
+    expect(print_str).to include(" -o media='A3'")
+
+    print.duplex = false
+    print.ppi = 'auto'
+    print_str = print.to_s
+    expect(print_str).to include(" -o duplex='one-sided'")
+    expect(print_str).to include(" -o ppi='auto'")
 
   end
 end
