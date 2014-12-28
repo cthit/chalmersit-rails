ee71542d5aedceded730c56247c95eecd1e50e6f
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-12-28 01:53:17 +0100

    feature specs for comments, posts and events

---
+++ spec/factories/event.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :event do |f|
+    event_date Date.today.tomorrow
+    full_day false
+    start_time Time.now + 1.hour
+    end_time Time.now + 2.hours
+    location 'Hubben 2.1'
+    organizer 'smurfarna'
+    association :post
+    facebook_link 'facebook.com/events/...'
+  end
+end
