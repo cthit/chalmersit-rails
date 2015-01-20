cad3f7d3cfdc3188db8324f59bdc683fc9dd52b2
Meddan <christoffer.medin@gmail.com>
2015-01-20 19:04:57 +0100

    Courses does no longer test for uniqueness of code.

--- spec/models/courses_spec.rb
+++ spec/models/courses_spec.rb
@@ -2,9 +2,4 @@ describe Course do
   it { should validate_presence_of :name }
   it { should validate_presence_of :code }
 
-  it do
-    FactoryGirl.create(:course)
-    should validate_uniqueness_of :code
-  end
-
 end
