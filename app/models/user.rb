ad2bb5f6a17e5e895fad8e5b15d9a2034216ae00
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-01-21 02:50:14 +0100

    Create fake user api, for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRestClient::Base
+  extend ActiveModel::Naming
   base_url Rails.configuration.account_ip
 
   before_request :set_bearer
 
-  get :all, "/users"
-  get :find, "/users/:id"
+  if Rails.env.test?
+    get :all, '/users', fake: [{uid: 'smurf'}, {uid: 'smurf2'}]
+    get :find, '/users/:id', fake: [{uid: 'smurf', display_name: 'Smurre smurf Smurfen'}]
+  else
+    get :all, '/users'
+    get :find, '/users/:id'
+  end
 
   def posts
     @posts ||= Post.find_by(user_id: uid)
