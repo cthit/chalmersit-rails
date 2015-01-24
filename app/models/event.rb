402646fb8514acbef88eaa622bc3bc195220d4e0
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-01-24 06:02:10 +0100

    What happens today?

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
+  scope :today, -> { where('`event_date` BETWEEN ? AND ?', Date.today.beginning_of_day, Date.tomorrow.beginning_of_day) }
   belongs_to :post
+
+  def start_date
+    event_date.to_time.change(hour: start_time.hour, min: start_time.min)
+  end
 end
