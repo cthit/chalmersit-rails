f35e29decdd0be612b869408c03938521a78e9f2
Adrian Bjugård <adrian@qng.se>
2015-04-19 18:23:51 +0200

    Added Einstein to lunch menu

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,6 +1,8 @@
 class LunchController < ApplicationController
   def feed
     @lunch = LunchModel.new
+    @restaurants = @lunch.chalmrest + @lunch.einstein
+    @restaurants.sort_by!{|r| r[:name]}
     render partial: 'feed'
   end
 end
