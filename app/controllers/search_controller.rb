5e39b5c00814f651bc930130a19ef396b29c7842
Cannonbait <ivar_busig@hotmail.com>
2015-07-28 23:40:26 +0200

    Search result now links to page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def index
     search = params[:search]
-    @posts = Post.with_translations(I18n.locale).where('post_translations.title LIKE ? or post_translations.body LIKE ?', "%#{search}%", "%#{search}%") 
+    posts_query = 'post_translations.title LIKE ? or post_translations.body LIKE ?'
+    @posts = Post.with_translations(I18n.locale).where(posts_query, "%#{search}%", "%#{search}%") 
+
 
   end
 end
