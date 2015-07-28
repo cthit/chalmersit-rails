e55e6b1b81264119a690578c565a525e550c7b0e
Cannonbait <ivar_busig@hotmail.com>
2015-07-28 23:20:30 +0200

    Search is now functional on posts #16

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
+    search = params[:search]
+    @posts = Post.with_translations(I18n.locale).where('post_translations.title LIKE ? or post_translations.body LIKE ?', "%#{search}%", "%#{search}%") 
 
   end
 end
