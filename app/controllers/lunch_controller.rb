215bdb5b098457d8b563261a0f5bf12fca6f29ca
Mattias Nilsen <kerp@chalmers.it>
2015-10-06 11:04:26 +0200

    Removed einstein from lunch feed

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,7 +1,7 @@
 class LunchController < ApplicationController
   def feed
     @lunch = LunchModel.new
-    @restaurants = @lunch.chalmrest + @lunch.einstein
+    @restaurants = @lunch.chalmrest
     @restaurants.sort_by!{|r| r[:name]}
   end
 end
