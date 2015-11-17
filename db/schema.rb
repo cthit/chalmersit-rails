cb771697ed8b568873a26bbd84a24561341e9cc0
Kalior <Kalioragus@gmail.com>
2015-11-18 00:42:18 +0100

    Created Contact model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702205539) do
+ActiveRecord::Schema.define(version: 20151110195140) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id",    limit: 4
@@ -56,6 +56,15 @@ ActiveRecord::Schema.define(version: 20150702205539) do
   end
 
   add_index "configurables", ["name"], name: "index_configurables_on_name", using: :btree
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
+    t.string   "email",      limit: 255
+    t.text     "to_whom",    limit: 65535
+  end
 
   create_table "course_translations", force: :cascade do |t|
     t.integer  "course_id",   limit: 4,     null: false
