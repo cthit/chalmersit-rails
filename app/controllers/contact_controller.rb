2d59e3ae297e9c9e24c89ffc6dbf0bbbe6c0f4a0
Kalior <kalioragus@gmail.com>
2016-02-07 21:06:30 +0100

    Make sure hidden input is empty?

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,21 +1,22 @@
 class ContactController < ApplicationController
 
 def index 
-	@contact = Contact.new
+   @contact = Contact.new
 end
 
 def create 
-	contact_params = params[:contact]
-	
-	mail_array = contact_params[:to_whom] << contact_params[:email]
+   if params[:value].blank? then
+      contact_params = params[:contact]
+      mail_array = contact_params[:to_whom] << contact_params[:email]
 
-	GroupMailer.anonymous_mail(mail_array, contact_params[:body], contact_params[:title]).deliver_now
-	
-	redirect_to :contact_index
+      GroupMailer.anonymous_mail(mail_array, contact_params[:body], contact_params[:title]).deliver_now
+   end
+
+   redirect_to :contact_index
 end
 
 def new
-	@contact = Contact.new
+   @contact = Contact.new
 end
 
 def show
