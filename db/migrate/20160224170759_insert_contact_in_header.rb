9fa045b6188fe12562aeeab80a8ed4dc17dda615
Kalior <kalioragus@gmail.com>
2016-02-24 19:16:04 +0100

    Revert "Add migration that adds kontakt to header"
    This reverts commit 342e52a66b30ea1d5055141cd50417e303952942.

--- db/migrate/20160224170759_insert_contact_in_header.rb
+++
@@ -1,7 +0,0 @@
-class InsertContactInHeader < ActiveRecord::Migration
-  def change
-    main_menu = Menu.where(name: "main").first
-    link = { controller: "contact", action: "index", title: "Kontakt", preferred_order: 3 }
-    MenuLink.create(link.merge(menu: main_menu))
-  end
-end
