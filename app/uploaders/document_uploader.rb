9436971ebc35777f7d77f8d62adbbe7e3f83634c
oskar367 <oskar.jedvert@gmail.com>
2016-05-04 02:21:02 +0200

    adds markdown to accepted files

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -39,7 +39,7 @@ class DocumentUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-   %w(pdf txt)
+   %w(pdf txt md)
   end
 
   # Override the filename of the uploaded files:
