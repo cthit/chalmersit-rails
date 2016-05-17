4dd0ca426e4cccffccc05483b0c4196177834bbf
oskar367 <oskar.jedvert@gmail.com>
2016-05-17 22:21:42 +0200

    adds sponsor model

---
+++ app/controllers/sponsors_controller.rb
@@ -0,0 +1,40 @@
+class SponsorsController < ApplicationController
+  before_action :set_sponsor, only: [:update, :edit, :show, :destroy]
+  def index
+    @sponsors = Sponsor.all
+  end
+  def show
+  end
+  def new
+    @sponsor = Sponsor.new
+  end
+  def create
+    @sponsor = Sponsor.new(sponsor_params)
+    if @sponsor.save
+      redirect_to sponsor_path(@sponsor)
+    else
+      render :new
+    end
+  end
+  def edit
+  end
+  def update
+    if @sponsor.update(sponsor_params)
+      redirect_to sponsor(@sponsor)
+    else
+      render :new
+    end
+  end
+  def destroy
+    @sponsor.destroy
+    redirect_to sponsors_path
+  end
+
+private
+  def sponsor_params
+    params.require(:sponsor).permit(:name, :image)
+  end
+  def set_sponsor
+    @sponsor = Sponsor.find(params[:id])
+  end
+end
---
+++ db/migrate/20160517195822_create_sponsors.rb
@@ -0,0 +1,10 @@
+class CreateSponsors < ActiveRecord::Migration
+  def change
+    create_table :sponsors do |t|
+      t.string :name
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
