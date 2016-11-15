97f5b2dd3156d584ed6117b1daa91b3f3c7eec37
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-11-15 22:07:16 +0100

    move locale scope with catch-all route to bottom

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,19 @@ Rails.application.routes.draw do
   # Redirects (necessary due to menu system only allowing inbound links)
   get 'redirect/findit' => 'redirect#findit'
 
+  resources :uploads, only: [:create, :destroy]
+
+  get 'twitter/feed/:twitter_handle' => 'twitter#feed'
+
+  post 'preview' => 'preview#preview'
+
+  get 'pages/:id/delete_document/:document_name', to: 'pages#delete_document', as: :delete_page_document
+  get 'posts/:id/delete_document/:document_name', to: 'posts#delete_document', as: :delete_post_document
+
+  match 'auth/:provider/callback' => 'sessions#create', via: [:get, :post]
+  get 'signout' => 'sessions#destroy', as: :signout
+
+
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     resources :committees, :contact
 
@@ -35,16 +48,4 @@ Rails.application.routes.draw do
     root 'home#index'
     get '*path' => 'pages#show'
   end
-  resources :uploads, only: [:create, :destroy]
-
-  get 'twitter/feed/:twitter_handle' => 'twitter#feed'
-
-  post 'preview' => 'preview#preview'
-
-  get 'pages/:id/delete_document/:document_name', to: 'pages#delete_document', as: :delete_page_document
-  get 'posts/:id/delete_document/:document_name', to: 'posts#delete_document', as: :delete_post_document
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-  match 'auth/:provider/callback' => 'sessions#create', via: [:get, :post]
-  get 'signout' => 'sessions#destroy', as: :signout
 end
