cb556c1caa9a3fddad1399c6d2f7ef7c24799f4c
Oskar Jedvert <oskar.jedvert@gmail.com>
2016-11-18 13:25:02 +0100

    localize title and body in pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,13 @@
 class Page < ActiveRecord::Base
+    translates :title, :body
+    globalize_accessors
+
     belongs_to :parent, class_name: 'Page'
     has_many   :children, class_name: 'Page'
     validate :notOwnParent
-    validates  :title, presence: true, allow_blank: false, uniqueness: { case_sensitive: true }
-    validates  :body, presence: true, allow_blank: false
+    #validates  :title_sv, presence: true, allow_blank: false, uniqueness: { case_sensitive: true }
+    #validates  :title_en, presence: true, allow_blank: false, uniqueness: { case_sensitive: true }
+    validates  *globalize_attribute_names, presence: true, allow_blank: false
     validates  :slug, allow_blank: false, uniqueness: { case_sensitive: true }, presence: true
 
     before_validation :generate_slug
