fa70ebbd2c4c9a4e2522636411a896ab5075d0f8
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-11-19 21:37:03 +0100

    Caching menulinks array instead of output

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,9 @@
 class Menu < ActiveRecord::Base
   has_many :links, class_name: 'MenuLink'
+
+  def self.links_for_menu_cached(menu_name)
+    Rails.cache.fetch("menus/#{menu_name}") do
+      Menu.where(name: menu_name).last.links.to_a
+    end
+  end
 end
