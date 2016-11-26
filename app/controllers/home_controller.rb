63a90da77c46ae16e7e057d163e647f1d6787db0
Kalior <kalioragus@gmail.com>
2016-11-26 15:46:08 +0100

    Add form for fetching student union card balance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,15 @@ class HomeController < ApplicationController
     @posts = policy_scope(Post).ordered.limit(10)
     @events = Event.today
   end
+
+  def card_balance
+    @balance = StudentUnionCardBalance.new.student_union_card_balance(card_balance_params[:number])
+    render partial: "card_balance"
+  end
+
+  private
+  def card_balance_params
+    params.permit(:number)
+  end
+
 end
