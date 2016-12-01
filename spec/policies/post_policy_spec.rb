4a087fe0545831bfee3c185affe70d6fc9b6ed85
Oskar Jedvert <oskar.jedvert@gmail.com>
2016-12-01 16:49:25 +0100

    add edit to separate test

--- spec/policies/post_policy_spec.rb
+++ spec/policies/post_policy_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe PostPolicy, type: :policy do
   subject { described_class }
 
-  permissions :update?, :edit? do
+  permissions :update? do
     it "should allow update if user in committee" do
       committee = 'nollkit'
       user = build(:committee_user)
       user.groups = [committee]
-      expect(subject).to permit(user, Post.new(group_id: committee))
+      post = build(:post)
+      post.group_id = committee
+      expect(subject).to permit(user, post)
     end
 
     it "should disallow update if user not in same committee" do
@@ -16,7 +18,30 @@ describe PostPolicy, type: :policy do
       other_committee = 'prit'
       user = build(:committee_user)
       user.groups = [committee]
-      expect(subject).not_to permit(user, Post.new(group_id: other_committee))
+      post = build(:post)
+      post.group_id = other_committee
+      expect(subject).not_to permit(user, post)
+    end
+  end
+
+  permissions :edit? do
+    it "should allow edit if user in committee" do
+      committee = 'nollkit'
+      user = build(:committee_user)
+      user.groups = [committee]
+      post = build(:post)
+      post.group_id = committee
+      expect(subject).to permit(user, post)
+    end
+
+    it "should disallow edit if user not in same committee" do
+      committee = 'nollkit'
+      other_committee = 'prit'
+      user = build(:committee_user)
+      user.groups = [committee]
+      post = build(:post)
+      post.group_id = other_committee
+      expect(subject).not_to permit(user, post)
     end
   end
 end
