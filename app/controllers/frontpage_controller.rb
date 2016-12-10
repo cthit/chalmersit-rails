46c7ae6115fdbfa2cb4faa4e1e02dadea362b627
Kalior <kalioragus@gmail.com>
2016-12-10 21:29:11 +0100

    Actually authorize frontpage

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -4,20 +4,26 @@ class FrontpageController < ApplicationController
   def update
      @frontpage = Frontpage.first
      @frontpage.update(frontpage_params)
-     
+
      @pages = Page.all
      render :edit
+     authorize_frontpage
   end
 
   def edit
     @frontpage = Frontpage.first
     @pages = Page.all
+    authorize_frontpage
   end
 
   private
 
     def frontpage_params
       params.require(:frontpage).permit([:page_id])
+    end
+
+    def authorize_frontpage
+      authorize @frontpage
     end
 
 end
