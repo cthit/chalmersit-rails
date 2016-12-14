4c5beac4dab96b29e741b927f3817acadbe8873c
Kalior <kalioragus@gmail.com>
2016-12-14 10:03:05 +0100

    Cache menu based on menu_links

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,7 +2,7 @@ class Menu < ActiveRecord::Base
   has_many :links, class_name: 'MenuLink'
 
   def self.links_for_menu_cached(menu_name)
-    Rails.cache.fetch(Menu.where(name: menu_name).last, expires_in: 24.hours) do
+    Rails.cache.fetch(Menu.where(name: menu_name).last.links) do
       Menu.where(name: menu_name).last.links.sort_by{|l| l.preferred_order}.to_a
     end
   end
