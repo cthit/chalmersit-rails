0ccf4c58995435539c6643f75f9348862817dbae
Kalior <kalioragus@gmail.com>
2016-12-14 10:23:34 +0100

    Make menu_links touch: true, cache only menu again

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,7 +2,7 @@ class Menu < ActiveRecord::Base
   has_many :links, class_name: 'MenuLink'
 
   def self.links_for_menu_cached(menu_name)
-    Rails.cache.fetch(Menu.where(name: menu_name).last.links) do
+    Rails.cache.fetch(Menu.where(name: menu_name).last) do
       Menu.where(name: menu_name).last.links.sort_by{|l| l.preferred_order}.to_a
     end
   end
