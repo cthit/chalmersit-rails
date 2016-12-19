33ac1a434d3e47de0b793dacb63d1bf437b4af0d
Kalior <kalioragus@gmail.com>
2016-12-19 20:47:21 +0100

    Hide sponsors page from !sponsor_admins

--- app/policies/sponsor_policy.rb
+++ app/policies/sponsor_policy.rb
@@ -1,5 +1,13 @@
 class SponsorPolicy < ApplicationPolicy
 
+  def index?
+    create?
+  end
+
+  def show?
+    create?
+  end
+
   def create?
     super || (user && user.committees_include_any?(Sponsor.sponsor_admins))
   end
