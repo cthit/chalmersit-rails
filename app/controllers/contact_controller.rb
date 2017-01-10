cb9745275310f18f240d59c64bfdbd5126bbffcf
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-01-10 16:25:36 +0100

    Fix erroneus comparison

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,27 +2,26 @@ class ContactController < ApplicationController
   layout 'bare', only: [:index, :show]
 
 
-def index
-   @contact = Contact.new
-end
+  def index
+     @contact = Contact.new
+  end
 
-def create
-   if params[:value].blank? && !params[:to_whom].blank? then
+  def create
       contact_params = params[:contact]
-      mail_array = contact_params[:to_whom] << contact_params[:email]
+      if params[:value].blank? && !contact_params[:to_whom].blank?
+        mail_array = contact_params[:to_whom] << contact_params[:email]
 
-      GroupMailer.anonymous_mail(mail_array, contact_params[:body], contact_params[:title]).deliver_now
-   end
+        GroupMailer.anonymous_mail(mail_array, contact_params[:body], contact_params[:title]).deliver_now
+      end
 
-   redirect_to :contact_index
-end
+     redirect_to :contact_index
+  end
 
-def new
-   @contact = Contact.new
-end
+  def new
+     @contact = Contact.new
+  end
 
-def show
-end
-
+  def show
+  end
 
 end
