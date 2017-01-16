ef1c7b0ccec25395571cba89d945cc3d1421dc82
Oskar Jedvert <oskar.jedvert@gmail.com>
2017-01-16 09:15:51 +0100

    adds meta tags to posts#show in order to allow for better unfurling

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,19 @@
 module PostsHelper
+  def post_meta post
+    body = markdown post.body
+    "<meta property=\"og:url\" content=\"#{post_url post}\"/>
+    <meta property=\"og:title\" content=\"#{post.title}\"/>
+    <meta property=\"og:description\" content=\"#{post.body}\"/>
+    <meta property=\"og:image\" content=\"#{get_first_image body}\"/>"
+  end
+  #get first image or set default
+  def get_first_image body
+    image = body[/img.*?src="(.*?)"/i,1]
+    if image.nil?
+      image = asset_path "logo-dark-txt.png"
+    end
+    image
+  end
   def show_event_fields?
     not @post.new_record? && @post.event?
   end
