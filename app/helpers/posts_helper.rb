7ad3480744d96e76a12a658891a08474dc241bd6
Oskar Jedvert <oskar.jedvert@gmail.com>
2017-01-16 09:32:44 +0100

    only use first paragraph for description

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
     body = markdown post.body
     "<meta property=\"og:url\" content=\"#{post_url post}\"/>
     <meta property=\"og:title\" content=\"#{post.title}\"/>
-    <meta property=\"og:description\" content=\"#{post.body}\"/>
+    <meta property=\"og:description\" content=\"#{first_paragraph body}\"/>
     <meta property=\"og:image\" content=\"#{get_first_image body}\"/>"
   end
   #get first image or set default
@@ -13,6 +13,9 @@ module PostsHelper
       image = asset_path "logo-dark-txt.png"
     end
     image
+  end
+  def first_paragraph body
+    body[/<p>(.*)<\/p>/i,1]
   end
   def show_event_fields?
     not @post.new_record? && @post.event?
