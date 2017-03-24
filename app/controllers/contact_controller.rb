38051e437d7ec3ca67ed57075b74a9dd637a99a1
xiaoming <henyan94@gmail.com>
2017-03-24 19:50:07 +0100

    end

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -10,6 +10,7 @@ class ContactController < ApplicationController
       contact_params = params[:contact]
       if params[:value].blank? && !contact_params[:to_whom].blank? && Contact.available_mails.include?(contact_params[:to_whom].first)
         mail_array = contact_params[:to_whom] << contact_params[:email]
+
         GroupMailer.anonymous_mail(mail_array, contact_params[:body], contact_params[:title]).deliver_now
       end
 
