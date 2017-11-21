a4fdfc05c09365b2c52dac766551d427bcd96269
Rasmus <rtmupp@gmail.com>
2017-11-21 21:19:33 +0100

    Order search based on date created

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     @search = params[:search]
     if @search
       posts_query = 'post_translations.title LIKE ? or post_translations.body LIKE ?'
-      @posts = Post.with_translations(I18n.locale).where(posts_query, "%#{@search}%", "%#{@search}%") 
+      @posts = Post.with_translations(I18n.locale).where(posts_query, "%#{@search}%", "%#{@search}%").ordered
     else
       @posts = []
     end
