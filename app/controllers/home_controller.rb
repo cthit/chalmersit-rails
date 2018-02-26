0e37f630fef5392ebdc1dca2d433ea94edd3ae89
Kalior <kalioragus@gmail.com>
2018-02-26 18:16:23 +0100

    Make the get more readable and correct

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,11 +12,16 @@ class HomeController < ApplicationController
   end
 
   def card_balance
-    url = Rails.configuration.card_balance_chalmers_it
+    uri = URI(Rails.configuration.card_balance_chalmers_it)
+    params = {number: card_balance_params[:number]}
+    uri.query = URI.encode_www_form(params)
     begin
       @balance, @name, @number = Rails.cache.fetch("card_balance/#{card_balance_params[:number]}", expires_in: 30.minutes) do
-        j = JSON.parse(Net::HTTP.get(URI("#{url}/?number=#{card_balance_params[:number]}")))
-        raise j["error"] if j.key?("error")
+        response = Net::HTTP.get_response(uri)
+        j = JSON.parse(response.body)
+
+        raise j["error"] if not response.is_a?(Net::HTTPSuccess)
+
         [j["balance"], j["name"], j["number"]]
       end
       render partial: "card_balance"
