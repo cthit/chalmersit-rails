8e5a13d431c9ac0a27176d895a4d0ae8980d48ed
davidm <damoller99@hotmail.se>
2019-10-14 22:03:18 +0200

    Chalmers.it now works with gotify

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -1,8 +1,26 @@
-class GroupMailer < ApplicationMailer
-	default from: 'no-reply@chalmers.it'
+require 'faraday'
+
+class GroupMailer
+	@sender_url = 'no-reply@chalmers.it'
   
-	def anonymous_mail(recipient, body, title)
-		  @body = body
-    	  mail(to: recipient, subject: title)
-	end
+	def self.anonymous_mail(recipient, body, title)
+    for i in recipient
+      if i != ""
+        link = "http://gotify:8080/mail"
+        url = URI.parse(link)
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = false
+        req = Net::HTTP::Post.new(url.path)
+        req.add_field('Content-Type', 'application/json')
+        req.add_field('Authorization', 'pre-shared: 123abc')
+        req.body = {
+          'to' => i,
+          'from' => @sender_url,
+          'subject' => title,
+          'body' => body
+        }.to_json
+        response = http.request(req)
+      end
+    end
+  end
 end
