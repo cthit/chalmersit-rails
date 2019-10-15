ec8d017aef212e344489beaa6c91c8601f002232
davidm <damoller99@hotmail.se>
2019-10-15 16:53:46 +0200

    Made gotify url and pre-shared key configurable via env-variable

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -6,13 +6,13 @@ class GroupMailer
 	def self.anonymous_mail(recipient, body, title)
     for i in recipient
       if i != ""
-        link = "http://gotify:8080/mail"
+        link = "#{ENV['GOTIFY_API_URL']}/mail"
         url = URI.parse(link)
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = false
         req = Net::HTTP::Post.new(url.path)
         req.add_field('Content-Type', 'application/json')
-        req.add_field('Authorization', 'pre-shared: 123abc')
+        req.add_field('Authorization', "pre-shared: #{ENV['GOTIFY_PRE_SHARED_KEY']}")
         req.body = {
           'to' => i,
           'from' => @sender_url,
