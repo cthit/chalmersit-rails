a2e1163ed3864335e4d392ba933c63bf0ce62eb6
molleer <damoller99@hotmail.se>
2019-12-10 13:32:06 +0100

    Added closing tag, contact use 2 space indentation, removed changes in db/schema.rb

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,5 +1,5 @@
 class ContactController < ApplicationController
-    def index
-        @contact_committees = Committee.where slug: ["styrIT","NollKIT","DPO","snIT","Valberedningen","digIT"]
-    end
+  def index
+    @contact_committees = Committee.where slug: ["styrIT","NollKIT","DPO","snIT","Valberedningen","digIT"]
+  end
 end
