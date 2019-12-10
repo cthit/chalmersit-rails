bfce07863e1bdac03b2fc60dfd7cc27c8c2d15a0
molleer <damoller99@hotmail.se>
2019-12-10 13:03:54 +0100

    Added suggestions from Malm

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,3 +1,5 @@
 class ContactController < ApplicationController
-
+    def index
+        @contact_committees = Committee.where slug: ["styrIT","NollKIT","DPO","snIT","Valberedningen","digIT"]
+    end
 end
