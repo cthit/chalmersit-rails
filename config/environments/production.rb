127177e6476fcdc340372d346482de2733ab8ec8
gurr1 <gustav.engsmyre@gmail.com>
2020-04-24 15:09:40 +0200

    should fix redis connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :redis_store, 'redis://0.0.0.0:6379/0/cache', { expires_in: 90.minutes }
+  config.cache_store = :redis_store, 'redis://redis:6379/0/', { expires_in: 90.minutes }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
